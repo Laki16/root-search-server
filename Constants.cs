90c594092549ae3fedbb19345316888d7cb9147b
Hanbum Lee <lkanen16@gmail.com>
2021-05-19 18:21:11 +0900

    Replace constant values with Constants class

---
+++ Constants.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ApiServer
+{
+	public static class Constants
+	{
+		/// <summary>
+		/// 추출할 연관 검색어 최대 개수
+		/// </summary>
+		public static int MaxAssociativeWords = 5;
+	}
+}
--- WorkerManager.cs
+++ WorkerManager.cs
@@ -115,7 +115,7 @@ namespace ApiServer
 
 				var sorted = scores.OrderByDescending(item => item.Value);
 
-				cached.AssociativeWords = sorted.Take(5).Select(x => x.Key).ToList();
+				cached.AssociativeWords = sorted.Take(Constants.MaxAssociativeWords).Select(x => x.Key).ToList();
 
 				// TODO: search failed handling
 				await _cache.SetAsync(keyword, cached);
