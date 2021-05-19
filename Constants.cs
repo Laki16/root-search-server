43c4065fe9e4cd63f757a95b26fea636fee16083
Hanbum Lee <lkanen16@gmail.com>
2021-05-20 01:21:13 +0900

    Add redis ttl constants

--- Constants.cs
+++ Constants.cs
@@ -18,5 +18,15 @@ namespace ApiServer
 		/// 블럭한 연관검색어가 만료되는 시각 (1일)
 		/// </summary>
 		public static TimeSpan blockExpireAfter = new TimeSpan(24, 0, 0);
+
+		/// <summary>
+		/// 레디스 캐시 Absolute TTL
+		/// </summary>
+		public static int RedisAbsoluteExpiration = 24;
+
+		/// <summary>
+		/// 레디스 캐시 비사용시 TTL
+		/// </summary>
+		public static int RedisSlidingExpiration = 15;
 	}
 }
